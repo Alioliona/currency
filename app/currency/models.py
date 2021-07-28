1ce783f36171caa885fcc1662c0819db78acef89
Alioliona <lenkaslastena@gmail.com>
2021-07-29 01:14:34 +0300

    created ContactUs

---
+++ app/currency/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Rate(models.Model):
+    currency_type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=50)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=30)
+    message = models.TextField()
